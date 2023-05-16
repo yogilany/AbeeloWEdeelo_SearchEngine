47c874fe93c4669ccf77b56e1ad7e5f81e115e67
yogilany <yousef.gilany021@eng-st.cu.edu.eg>
2023-05-16 14:36:00 +0300

    Publish

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
+
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
